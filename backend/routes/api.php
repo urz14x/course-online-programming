ad6d3d006f1b66fc03ce35894d13ead06a971f93
Ustami Razib <ustamirazib45@gmail.com>
2022-12-03 08:31:42 +0700

    menambahkan controller admin

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,10 +1,13 @@
 <?php
 use App\Http\Controllers\Auth\{RegisterController, LoginController, ProfileController, LogoutController};
+use Illuminate\Support\Facades\Route;
 
 Route::post("/register", RegisterController::class);
 Route::post("/login", LoginController::class);
-
 Route::middleware('auth:sanctum')->group(function () {
     Route::get("/profile", ProfileController::class);
     Route::post('/logout', LogoutController::class);
 });
+
+//Admin 
+Route::post('/admin/register',);
